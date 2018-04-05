737eace1d6cab0fb1e7248aaf1780748c0ff3670
jirons44 <joeirons@comcast.net>
2018-04-05 13:41:11 -0500

    Heroku

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fullStack_server
+
+###### deployed on Heroku:  
+> must have:
+ -dynamic PORT binding, 
+ -specify NODE environment, 
+ -start script
+ -.gitignore for node_modules
+ 
+ 
--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-
     res.send({hi: 'hi there'});
-    
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
